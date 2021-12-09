d38fca13a532ee3bf54a81a78d274d1642929598
Hajar Aswad <hajaraswadkom@gmail.com>
2021-12-09 14:59:14 +0800

    Container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
 
-import './App.css';
-
+import {
+    Header,
+    About,
+    Services,
+    Experience,
+    Project,
+    CTA,
+    Footer,
+} from "./containers";
+import "./App.css";
 
 const App = () => {
-     return (
-          <div className="App">
-               cek
-          </div>
-     )
+    return (
+        <div className="App">
+            <Header />
+            <About />
+            <Services />
+            <Experience />
+            <Project />
+            <CTA />
+            <Footer />
+        </div>
+    );
 };
 
-
 export default App;
-
---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+export { default as Header } from './header/Header';
+export { default as About } from './about/About';
+export { default as Services } from './services/Services';
+export { default as Experience } from './experience/Experience';
+export { default as Project } from './project/Project';
+export { default as CTA } from './cta/CTA';
+export { default as Footer } from './footer/Footer';
