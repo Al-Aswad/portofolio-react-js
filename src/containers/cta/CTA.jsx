7d16cd44c40388869be5b70b1d721fa5276d10fd
Hajar Aswad <hajaraswadkom@gmail.com>
2021-12-11 01:56:31 +0800

    cta

--- src/containers/cta/CTA.jsx
+++ src/containers/cta/CTA.jsx
@@ -3,11 +3,22 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => {
-  return(
-        <div>
-             CTA
-          </div>
-  )
+  return (
+    <div className="cta__container section__padding">
+      <div className="cta__container-content">
+        <div className="cta-content">
+          <h2>Lets Work Together</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur tempora autem quam alias! Cumque, omnis.
+          </p>
+        </div>
+        <div className="cta-button">
+          <button type="button">Contact Us</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
