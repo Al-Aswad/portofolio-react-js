3d6639198ee89f695368225c9f62d625d6c6f120
Hajar Aswad <hajaraswadkom@gmail.com>
2021-12-11 01:46:25 +0800

    project

--- src/containers/project/Project.jsx
+++ src/containers/project/Project.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-
-import './project.css'
+import React from "react";
+import { Heading } from "../../components";
+import projectImg from "../../assets/img/project.png";
+import "./project.css";
 
 const Project = () => {
-  return(  
-        <div>
-          Project
-     </div>
-  )
+  return (
+    <div className="project__container section__padding">
+      <Heading title="Project" subTitle="Latest Project" />
+      <div className="project__container-content__container">
+        <div className="project__container-content">
+          <div className="project__card">
+            <img src={projectImg} alt="project" />
+          </div>
+          <h4>HR(Payroll)</h4>
+          <p>2020</p>
+        </div>
+        <div className="project__container-content">
+          <div className="project__card">
+            <img src={projectImg} alt="project" />
+          </div>
+          <h4>HR(Payroll)</h4>
+          <p>2020</p>
+        </div>
+        <div className="project__container-content">
+          <div className="project__card">
+            <img src={projectImg} alt="project" />
+          </div>
+          <h4>HR(Payroll)</h4>
+          <p>2020</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
