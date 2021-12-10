2a58dd9c22fd65f6c8f288de794459147aabb781
Hajar Aswad <hajaraswadkom@gmail.com>
2021-12-10 08:25:26 +0800

    about progress

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import "./heading.css";
 
 const Heading = () => {
-  return(
-          <div>
-             Heading
-          </div>
-  )
+  return (
+    <div className="heading">
+      <h4>About Us</h4>
+      <h1>Why You Hire me?</h1>
+      <div />
+    </div>
+  );
 };
 
 export default Heading;
--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,13 +1,37 @@
 import React from "react";
+import Heading from "../../components/heading/Heading";
 
+import People from "../../assets/img/people.png";
 import "./about.css";
 
 const About = () => {
-  return(
-        <div>
-             About
+  return (
+    <div className="about__container section__padding">
+      <div className="about__container-left">
+        <div className="about__container-left__img">
+          <img src={People} alt="people" />
+        </div>
+      </div>
+      <div className="about__container-right">
+        <Heading />
+        <div className="about__container-right__description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
+            obcaecati, corporis similique cupiditate omnis sint?
+          </p>
+        </div>
+        <div className="about__container-right__skils-container">
+          <div className="about__container-right__skils-container-skill">
+            <div className="about__container-right__skils-container-skill-title">
+              <h4>HTML</h4>
+              <h4>80</h4>
+            </div>
+            <progress value="80" max="100"></progress>
           </div>
-  )
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
